be5d4a810a932c634b5456ca1e845c51ebd694d9
lksawyer <loganksawyer@gmail.com>
2018-01-24 20:40:37 -0500

    added html-routes.js

---
+++ routes/html-routes.js
@@ -0,0 +1,14 @@
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  //
+  app.get("/", function(req, res) {
+    res.render("hi");
+  });
+
+};
