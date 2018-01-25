e1045e2ce452fefd459dc91ab5092fa66d96b23a
lksawyer <loganksawyer@gmail.com>
2018-01-24 20:25:11 -0500

    added server.js and routes directory

---
+++ server.js
@@ -0,0 +1,38 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Allows Express app to handle data parsing
+// =============================================================
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Allows Express app tp serve static files in the public folder
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+require("./routes/author-api-routes.js")(app);
+require("./routes/post-api-routes.js")(app);
+
+// Require models for sequelize syncing
+// =============================================================
+var db = require("./models");
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
