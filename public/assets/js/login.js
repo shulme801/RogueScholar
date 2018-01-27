55c55378f7a2a8096f81d25d63743f698d64283d
lksawyer <loganksawyer@gmail.com>
2018-01-27 12:28:37 -0500

    created login.html, login.js, and updated user-api-routes to get all users

---
+++ public/assets/js/login.js
@@ -0,0 +1,28 @@
+$("#login").on("click", function(event) {
+  event.preventDefault();
+
+  // Grab the form elements
+  var username = {
+    username: $("#username").val().trim(),
+  };
+
+  console.log(username);
+
+  // This line is the magic. It"s very similar to the standard ajax function we used.
+  // Essentially we give it a URL, we give it the object we want to send, then we have a "callback".
+  // The callback is the response of the server. In our case, we set up code in api-routes that "returns" true or false
+  // depending on if a tables is available or not.
+
+  $.get("/api/user", username,
+    function(data) {
+
+    console.log(data);
+
+    // Clear the form when submitting
+    $("#username").val("");
+
+  });
+
+});
+
+
