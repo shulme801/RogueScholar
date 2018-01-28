43f794f0b88d5fe3ce026a6a430ff54b59571d58
lksawyer <loganksawyer@gmail.com>
2018-01-28 15:00:07 -0500

    updated login, can now create post

---
+++ public/assets/js/createPost.js
@@ -0,0 +1,29 @@
+$("#create_post").on("click", function(event) {
+  event.preventDefault();
+
+  // Grab the form elements
+  var post = {
+    user_user_id: localStorage.getItem("user_id"),
+    content: $("textarea[name='content']").val().trim(),
+  };
+
+  console.log("form post input: ", post);
+
+  // Post form data to our api route that handles user login
+  $.post("/api/create_post", post,
+    function(data) {
+
+    console.log("Data from db ", data);
+
+    if(data) {
+      console.log("Successfully created post");
+    } else {
+      console.log("Failed to create post");
+    }
+
+    // Clear the form when submitting
+    $("input[name='username']").val("");
+
+  });
+
+});
--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -2,11 +2,12 @@ var db = require("../models");
 
 module.exports = function(app) {
 
+	// create post that is assocaited with the user_id stored in local storage
 	app.post("/api/create_post", function(req, res) {
 		console.log(req.body);
 		db.Posts.create({
+			user_user_id: req.body.user_user_id,
 			content: req.body.content,
-			user_user_id: 1,
 		}).then(function(dbUser) {
 			res.json(dbUser);
 		});
