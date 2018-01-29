66350696be72e4d17a5419476a4c22b36a2cedc3
shulme801 <shulme801@gmail.com>
2018-01-28 19:13:35 -0500

    28 Jan Hulme -- comments can be input into DB

---
+++ public/assets/js/createComment.js
@@ -0,0 +1,31 @@
+// Creates a comment and associates it with a post
+
+$("#create_comment").on("click", function(event) {
+  event.preventDefault();
+
+  // Grab the form elements
+  var comment = {
+    post_post_id: localStorage.getItem("post_id"),
+    content: $("textarea[name='content']").val().trim(),
+  };
+
+  console.log("form comment input: ", comment);
+
+  // Post form data to our api route that handles create comment
+  $.post("/api/create_comment", comment,
+    function(data) {
+
+    console.log("Data from db ", data);
+
+    if(data) {
+      console.log("Successfully created comment");
+    } else {
+      console.log("Failed to create comment");
+    }
+
+    // Clear the form when submitting
+    $("input[name='create_comment']").val("");
+
+  });
+
+});
--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -2,11 +2,22 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-	// create post that is assocaited with the user_id stored in local storage
+	// create post that is associated with the user_id stored in local storage
 	app.post("/api/create_post", function(req, res) {
 		console.log(req.body);
 		db.Posts.create({
 			user_user_id: req.body.user_user_id,
+			content: req.body.content,
+		}).then(function(dbUser) {
+			res.json(dbUser);
+		});
+	});
+
+   // create comment that is associated with the comment_id stored in local storage
+   app.post("/api/create_comment", function(req, res) {
+		console.log(req.body);
+		db.Comments.create({
+			post_post_id: req.body.post_post_id,
 			content: req.body.content,
 		}).then(function(dbUser) {
 			res.json(dbUser);
