f5db547acb2856fc8adb8cef6342d3c52641aa28
shulme801 <shulme801@gmail.com>
2018-01-29 19:26:40 -0500

    29 Jan Added routes to retrieve posts and comments

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -2,6 +2,8 @@ var db = require("../models");
 
 module.exports = function(app) {
 
+
+/* Here are the database create record routes */
 	// create post that is associated with the user_id stored in local storage
 	app.post("/api/create_post", function(req, res) {
 		console.log(req.body);
@@ -24,6 +26,70 @@ module.exports = function(app) {
 		});
 	});
 
+/* here are the database retrieve routes */
 
+// GET route for getting all of the posts
+  app.get("/api/posts/", function(req, res) {
+    db.Post.findAll({})
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+// GET route for getting all of the comments
+  app.get("/api/comments/", function(req, res) {
+    db.Comments.findAll({})
+    .then(function(dbComments) {
+      res.json(dbComments);
+    });
+  });
+
+  // Get route for returning posts of a specific category
+  app.get("/api/posts/category/:category", function(req, res) {
+    db.Post.findAll({
+      where: {
+        category: req.params.category
+      }
+    })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // Get route for returning comments of a specific category
+  app.get("/api/posts/category/:category", function(req, res) {
+    db.Comments.findAll({
+      where: {
+        category: req.params.category
+      }
+    })
+    .then(function(dbComments) {
+      res.json(dbComments);
+    });
+  });
+
+  // Get route for retrieving a single post
+  app.get("/api/posts/:id", function(req, res) {
+    db.Post.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // Get route for retrieving a single comment
+  app.get("/api/comments/:id", function(req, res) {
+    db.Comments.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(function(dbComments) {
+      res.json(dbComments);
+    });
+  });
 
 };
